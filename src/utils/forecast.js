dd54e1d5096b1449e7e5826460ab4237eadf5e5f
Ainafee Maknojia <ainafee@Ainafees-MacBook-Pro.local>
2021-01-29 15:58:46 -0500

    Init commit

---
+++ src/utils/forecast.js
@@ -0,0 +1,17 @@
+const request = require('request')
+
+const forecast = (longitude, latitude, callback) => {
+    const url = 'http://api.weatherstack.com/current?access_key=32a773192bfa0de8c1db5673968fe07c&query=' + latitude + ',' + longitude + '&units=m'
+
+    request({url, json: true}, (error, { body }) => {
+        if (error) {
+            callback('Unable to connect to weather service!', undefined)
+        } else if (body.error) {
+            callback('Unable to find location', undefined)
+        } else {
+            callback(undefined, body.current.weather_descriptions[0] + '. It is currently ' + body.current.temperature + ' degrees out. It feels like ' + body.current.feelslike + ' degrees out.')
+        }
+    })
+}
+
+module.exports = forecast
